8e46def1481598f302ddd4012b02c3660ebd4c66
ArmanHC <arman.seyri@gmail.com>
2021-01-18 14:55:03 +0330

    Add files via upload

---
+++ Install requierements.bat
@@ -0,0 +1,38 @@
+@shift /0
+@echo off
+title Adobe RPC By @Arman_HC / @Arman2122 [1.0]
+color 8
+@echo ===========================================================================
+@echo(
+@echo          # #                   # #        
+@echo        # #     # # # # # # #     # #      
+@echo      # # # # # # # # # # # # # # # # #    
+@echo      # # # # # # # # # # # # # # # # #    
+@echo      # # # # # # # # # # # # # # # # #    
+@echo    # # # # # # # # # # # # # # # # # # #   ==== Version: [ 1.0 ]====
+@echo    # # # # # # # # # # # # # # # # # # #  
+@echo    # # # # #     # # # # #     # # # # #  
+@echo    # # # #         # # #         # # # #  
+@echo  # # # # #         # # #         # # # # #    /### ADOBE RPC ###\
+@echo  # # # # # #     # # # # #     # # # # # #
+@echo  # # # # # # # # # # # # # # # # # # # # #
+@echo  # # # # # # # # # # # # # # # # # # # # #
+@echo  # # # # #     # # # # # # #     # # # # #
+@echo      # # # #                   # # # #    
+@echo        # # # #               # # # #
+@echo ___________________________________________________________________________ 
+@echo   So 
+@echo   https://www.github.com/arman2122           Chek It Out Also
+@echo(
+@echo ===========================================================================
+@echo(
+@echo(
+@echo Installing Python Requierements
+@echo(
+@echo(
+python -m pip install --upgrade pip
+py -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+py -m pip install -r requirements.txt
+echo Installed !
+pause
